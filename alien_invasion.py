73fe602138b729afacd50b8b120d5fb9c96fd335
lanefrasier <lanefrasier22@gmail.com>
2024-06-26 14:49:01 -0500

    moving right

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -35,8 +36,10 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    #Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
             """Upadate images on the scrren, and flip to the new screen"""
