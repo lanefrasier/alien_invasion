96ca045b18d5b136a9a78a10ca0f0d5a7120de64
Lanefrasier <lanefrasier22@gmail.com>
2024-07-14 13:51:09 -0500

    finished coding

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,8 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             #Get rid of any remaining bullets and aliens
@@ -140,12 +142,17 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             #Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            #Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Check if the fleet is at an adge, then update positions"""
@@ -208,8 +215,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            #Decrement ships left
+            #Decrement ships_left, and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             #Get rid of any remaining bullets and aliens
             self.bullets.empty()
             self.aliens.empty()
--- game_stats.py
+++ game_stats.py
@@ -13,3 +13,4 @@ class GameStats:
         """Initialize statistics that can chane during the game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
